3ecef533f4c7969ef8a7111134f08971a95efd82
Andy Phan <andyphan@live.com.au>
2024-06-05 21:43:04 +0930

    Create CustomUser model. Add VSCode configs

---
+++ networth_tracker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NetworthTrackerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'networth_tracker'
---
+++ networth_tracker/models.py
@@ -0,0 +1,31 @@
+# networth_tracker/models.py
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Specifies whether the user can log into the Django admin site"),
+    )
+    is_active = models.BooleanField(_("active status"), default=True)
+    is_verified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now())
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    def __str__(self):
+        return self.email
