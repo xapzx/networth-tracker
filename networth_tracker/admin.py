782bd4f729f0a43f50e5eacccc25c28c570f2d8a
Andy Phan <andyphan@live.com.au>
2024-06-10 11:40:22 +0930

    Add custom CustomUser admin page

--- networth_tracker/admin.py
+++ networth_tracker/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            None,
+            {"fields": ("email", "password", "is_verified")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Dates", {"fields": ("date_joined", "last_login")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
+
+    model = models.CustomUser
+    ordering = ["-date_joined"]
+    list_display = [
+        "email",
+        "pk",
+        "is_verified",
+        "date_joined",
+        "last_login",
+        "is_staff",
+        "is_active",
+    ]
+    list_filter = ["is_verified", "is_staff"]
+    filter_horizontal = ["groups", "user_permissions"]
+    readonly_fields = ["date_joined", "last_login", "password"]
